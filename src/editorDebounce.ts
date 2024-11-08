725e52c8d94b7e7ab9723f5b2668adc553ddf6e7
SR_team <me@sr.team>
2024-11-09 02:11:05 +0500

    Recolorize editor, when semantic tokens not provided

--- src/editorDebounce.ts
+++ src/editorDebounce.ts
@@ -1,18 +1,21 @@
 import * as vscode from 'vscode'
 
-type EditorFunction = (editor: vscode.TextEditor) => void
+type EditorFunction = (editor: vscode.TextEditor) => Promise<boolean>
 
-export function debounce(func: EditorFunction, delay: number): EditorFunction {
+export function debounce(func: EditorFunction, delay: number): (editor: vscode.TextEditor) => void {
     const lastExecutionTimes = new Map<string, number>()
 
-    return (editor: vscode.TextEditor) => {
+    return async (editor: vscode.TextEditor) => {
         const now = Date.now()
         const key = editor.document.uri.toString()
         const lastExecution = lastExecutionTimes.get(key) || 0
 
         if (now - lastExecution >= delay) {
-            func(editor)
-            lastExecutionTimes.set(key, now)
+            if (await func(editor)) {
+                lastExecutionTimes.set(key, now)
+            } else {
+                setTimeout(() => debounce(func, delay)(editor), delay)
+            }
         }
     }
 }
